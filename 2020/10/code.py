700401f022eaa32341acb5f87f4f0b805b9c1dd4
Ravi Tellakula <ravi.tellakula@aa.com>
2020-12-10 12:31:13 -0600

    Day10

---
+++ 2020/10/code.py
@@ -0,0 +1,54 @@
+from os import read
+import time
+
+def readInputFile(fileName) :
+  with open(f"{__file__.rstrip('code.py')}{fileName}",'r') as f :
+    return [int(line.strip()) for line in f.read().splitlines()]
+
+
+def diffCombinations(inputList):
+  numcombinations = {0:1}
+
+  for volt in inputList :
+    numcombinations[volt] = 0
+    if volt - 1 in numcombinations:
+      numcombinations[volt] += numcombinations[volt-1]
+    if volt - 2 in numcombinations:
+      numcombinations[volt] += numcombinations[volt-2]
+    if volt - 3 in numcombinations:
+      numcombinations[volt] += numcombinations[volt-3]
+
+  return numcombinations[max(inputList)]
+
+def getVolts(inputList) :
+  volt1Count, volt3Count = 1 , 1
+  currentVoltage = inputList[0]
+
+  for index in range(len(inputList)) :
+    if (currentVoltage + 1) in inputList :
+      volt1Count += 1
+      currentVoltage += 1
+    elif (currentVoltage + 3) in inputList :
+      volt3Count += 1
+      currentVoltage += 3
+    else :
+      return volt1Count * volt3Count
+
+def test():
+  inputlist = sorted(readInputFile("example.txt"))
+  assert getVolts(inputlist) == 35
+  assert diffCombinations(inputlist) == 8
+
+
+def main():
+  inputlist = sorted(readInputFile("input.txt"))
+  print("Multiply of 1-volt and 3-volt - ", getVolts(inputlist))
+  print("Total no. of Combinations - ", diffCombinations(inputlist))
+
+if __name__ == "__main__" :
+  test_start_time = time.time()
+  test()
+  print("Total Execution Time [{}] seconds for Test".format(time.time() - test_start_time))
+  main_start_time = time.time()
+  main()
+  print("Total Execution Time [{}] seconds for Test".format(time.time() - main_start_time))
